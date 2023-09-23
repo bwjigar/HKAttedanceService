9acddd95471d01017b93e436998c66e3653c58f0
Rajeshri <yami@brainwaves.co.in>
2023-09-23 14:52:44 +0530

    commit

---
+++ HKAttedanceService/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using HKAttedanceService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HKAttedanceService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public JsonResult TransferAttendance()
+        {
+            CommonResponse CR = new CommonResponse();
+            try
+            {
+                CR = Repository.TransferDataSQLToORA();
+                CR = Repository.TransferDataSQLToORA7Days();
+                return Json(CR, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                CR.Message = "Something Went Wrong.";
+                CR.Error = ex.Message.ToString();
+                CR.Status = "0";
+                return Json(CR, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ HKAttedanceService/Models/Repository.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace HKAttedanceService.Models
+{
+    public static class Repository
+    {
+        public static CommonResponse TransferDataSQLToORA()
+        {
+            IFormatProvider theCultureInfo = new System.Globalization.CultureInfo("en-GB", true);
+            CommonResponse resp = new CommonResponse();
+            try
+            {
+                Database db = new Database();
+                List<IDbDataParameter> para;
+                para = new List<IDbDataParameter>();
+                DataTable dt = db.ExecuteSP("TransferAttendanceDetail_Ora", para.ToArray(), false);
+
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    if (dt.Rows[0]["Msg"].ToString() == "Success")
+                    {
+                        resp.Message = "Attendance Transfer Successfully";
+                        resp.Status = "1";
+                    }
+                    else
+                    {
+                        resp.Message = "Something Went Wrong.";
+                        resp.Error = dt.Rows[0]["Msg"].ToString();
+                        resp.Status = "0";
+                    }
+                }
+                else
+                {
+                    resp.Message = "Something Went Wrong.";
+                    resp.Error = "Data Not Found";
+                    resp.Status = "0";
+                }
+                return resp;
+            }
+            catch (Exception ex)
+            {
+                resp.Message = "Something Went Wrong.";
+                resp.Error = ex.Message.ToString();
+                resp.Status = "0";
+            }
+            return resp;
+        }
+        public static CommonResponse TransferDataSQLToORA7Days()
+        {
+            IFormatProvider theCultureInfo = new System.Globalization.CultureInfo("en-GB", true);
+            CommonResponse resp = new CommonResponse();
+            try
+            {
+                Database db = new Database();
+                List<IDbDataParameter> para;
+                para = new List<IDbDataParameter>();
+                DataTable dt = db.ExecuteSP("TransferAttendanceDetail_7Days_Ora", para.ToArray(), false);
+
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    if (dt.Rows[0]["Msg"].ToString() == "Success")
+                    {
+                        resp.Message = "Attendance Transfer Successfully";
+                        resp.Status = "1";
+                    }
+                    else
+                    {
+                        resp.Message = "Something Went Wrong.";
+                        resp.Error = dt.Rows[0]["Msg"].ToString();
+                        resp.Status = "0";
+                    }
+                }
+                else
+                {
+                    resp.Message = "Something Went Wrong.";
+                    resp.Error = "Data Not Found";
+                    resp.Status = "0";
+                }
+                return resp;
+            }
+            catch (Exception ex)
+            {
+                resp.Message = "Something Went Wrong.";
+                resp.Error = ex.Message.ToString();
+                resp.Status = "0";
+            }
+            return resp;
+        }
+    }
+}
